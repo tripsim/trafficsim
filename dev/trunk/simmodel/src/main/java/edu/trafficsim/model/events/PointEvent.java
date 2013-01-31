ad75f6a838f3ba6ca53267a25e72dccf01cfc0ec
xuans <xuans@e140655a-d0db-44a3-a22e-ea662fbe9d59>
2013-01-31 05:08:04 +0000

    Updated event abstract classes.

--- dev/trunk/simmodel/src/main/java/edu/trafficsim/model/events/PointEvent.java
+++ dev/trunk/simmodel/src/main/java/edu/trafficsim/model/events/PointEvent.java
@@ -1,14 +1,19 @@
 package edu.trafficsim.model.events;
 
-public abstract class PointEvent extends Event<PointEvent> {
+import com.vividsolutions.jts.geom.Coordinate;
+
+import edu.trafficsim.model.core.AbstractLocation;
+
+public abstract class PointEvent<T> extends AbstractLocation<T> {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public PointEvent(EventType eventType) {
-		super(eventType);
+	public PointEvent(Coordinate coord) {
+		super(coord);
 	}
 	
+	// TODO add timewindow to accomodate common features of events
 }
