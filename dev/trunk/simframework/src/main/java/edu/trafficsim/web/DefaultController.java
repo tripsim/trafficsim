99e6352c65a8914f33f801be2d8d3a91e9e01a1f
xuans <xuans@e140655a-d0db-44a3-a22e-ea662fbe9d59>
2013-04-18 22:21:13 +0000

    Servelet updates.

--- dev/trunk/simframework/src/main/java/edu/trafficsim/web/DefaultController.java
+++ dev/trunk/simframework/src/main/java/edu/trafficsim/web/DefaultController.java
@@ -3,16 +3,12 @@ package edu.trafficsim.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class DefaultController {
 
-	@Controller
-	public class HomeController {
-	 
-	    @RequestMapping(value = "/")
-	    public String home() {
-	        System.out.println("HomeController: Passing through...");
-	        return "WEB-INF/views/home.jsp";
-	    }
+	@RequestMapping(value = "/")
+	public String home() {
+		System.out.println("HomeController: Passing through...");
+		return "index";
 	}
 }
