22a98d63d98eb2e76b57ea481de3d8d2a9e9e58f
Xuan Shi <bennieys@gmail.com>
2013-04-25 20:55:46 +0000

    Fixed map zoom levels.

--- dev/trunk/simframework/src/main/webapp/scripts/simulation.js
+++ dev/trunk/simframework/src/main/webapp/scripts/simulation.js
@@ -33,21 +33,38 @@ simulation.initMap = function() {
 
 	var map = this.map = new OpenLayers.Map('map', {
 		projection : proj900913,
-		allOverlays : true
+		allOverlays : true,
+		fractionalZoom : true
 	});
-	var osm = new OpenLayers.Layer.OSM();
+	var osm = new OpenLayers.Layer.OSM(null, null, {
+		resolutions : [ 156543.03390625, 78271.516953125, 39135.7584765625,
+				19567.87923828125, 9783.939619140625, 4891.9698095703125,
+				2445.9849047851562, 1222.9924523925781, 611.4962261962891,
+				305.74811309814453, 152.87405654907226, 76.43702827453613,
+				38.218514137268066, 19.109257068634033, 9.554628534317017,
+				4.777314267158508, 2.388657133579254, 1.194328566789627,
+				0.5971642833948135, 0.25, 0.1, 0.05 ],
+		serverResolutions : [ 156543.03390625, 78271.516953125,
+				39135.7584765625, 19567.87923828125, 9783.939619140625,
+				4891.9698095703125, 2445.9849047851562, 1222.9924523925781,
+				611.4962261962891, 305.74811309814453, 152.87405654907226,
+				76.43702827453613, 38.218514137268066, 19.109257068634033,
+				9.554628534317017, 4.777314267158508, 2.388657133579254,
+				1.194328566789627, 0.5971642833948135 ],
+		transitionEffect : 'resize'
+	});
 	var gmap = new OpenLayers.Layer.Google('Google Streets', {
 		numZoomLevels : 20,
 		visibility : false
 	});
 	var gsat = new OpenLayers.Layer.Google("Google Satellite", {
 		type : google.maps.MapTypeId.SATELLITE,
-		numZoomLevels : 22,
+		numZoomLevels : 20,
 		visibility : false
 	});
 	var ghyb = new OpenLayers.Layer.Google("Google Hybrid", {
 		type : google.maps.MapTypeId.HYBRID,
-		numZoomLevels : 22,
+		numZoomLevels : 21,
 		visibility : false
 	});
 	map.addLayers([ osm, gmap, gsat, ghyb ]);
