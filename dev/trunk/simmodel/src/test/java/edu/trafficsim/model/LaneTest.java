dfd66326626851fd2d7499c2f0e557750f138eff
xuans <xuans@e140655a-d0db-44a3-a22e-ea662fbe9d59>
2014-03-05 22:41:25 +0000

    Fixed junit test.

--- dev/trunk/simmodel/src/test/java/edu/trafficsim/model/LaneTest.java
+++ dev/trunk/simmodel/src/test/java/edu/trafficsim/model/LaneTest.java
@@ -3,11 +3,13 @@ package edu.trafficsim.model;
 import java.lang.reflect.Field;
 
 import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.junit.Before;
 import org.junit.Test;
 import org.opengis.referencing.operation.TransformException;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
 
 import edu.trafficsim.model.VehicleType.VehicleClass;
 import edu.trafficsim.model.core.AbstractSegment;
@@ -22,14 +24,30 @@ public class LaneTest {
 
 	GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
 
+	Coordinate coord1;
+	Coordinate coord2;
+	Coordinate coord3;
+	Coordinate[] coords;
+	LineString lineString;
+
+	@Before
+	public void before() {
+		coord1 = new Coordinate(0, 0);
+		coord2 = new Coordinate(10, 10);
+		coord3 = new Coordinate(30, 30);
+		coords = new Coordinate[] { coord1, coord2, coord3 };
+		lineString = geometryFactory.createLineString(coords);
+	}
+
 	@Test
 	public void testNavigable() throws ModelInputException, TransformException,
 			NoSuchMethodException, SecurityException, NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 
 		DefaultLink link = new DefaultLink(0, null, null, new DefaultNode(0,
-				null, null, null), new DefaultNode(0, null, null, null),
-				geometryFactory.createLineString(new Coordinate[] {}), null);
+				null, null, geometryFactory.createPoint(coord1)),
+				new DefaultNode(0, null, null, geometryFactory
+						.createPoint(coord3)), lineString, null);
 
 		Field f1 = AbstractSegment.class.getDeclaredField("length");
 		f1.setAccessible(true);
@@ -38,14 +56,15 @@ public class LaneTest {
 
 		f1.set(link, 5);
 
-		Lane lane = new DefaultLane(0, link, 0, 1, 2);
+		Lane lane = new DefaultLane(0, link, 0, 0, 0);
 		VehicleType vehicleType = new VehicleType(0, "Test", VehicleClass.Car);
+		DriverType driverType = new DriverType(0, "Test");
 		DefaultVehicle vehicle1 = new DefaultVehicle(0, null, 0, vehicleType,
-				null);
+				driverType);
 		vehicle1.setName("first");
 		vehicle1.position(10);
 		DefaultVehicle vehicle2 = new DefaultVehicle(1, null, 0, vehicleType,
-				null);
+				driverType);
 		vehicle2.setName("second");
 		vehicle2.position(2);
 
