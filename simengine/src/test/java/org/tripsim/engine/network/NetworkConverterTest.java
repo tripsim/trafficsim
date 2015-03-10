8ba27cda43c76bca2cace3f830775e90f5e804c2
Xuan Shi <bennieys@gmail.com>
2015-03-09 22:34:30 -0700

    updated license info

--- simengine/src/test/java/org/tripsim/engine/network/NetworkConverterTest.java
+++ simengine/src/test/java/org/tripsim/engine/network/NetworkConverterTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2015 Xuan Shi
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.a
+ * 
+ * @author Xuan Shi
+ */
 package org.tripsim.engine.network;
 
 import org.junit.Test;
